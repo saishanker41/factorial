c63c24df0fa109117def4b1360163da7a5bf2026
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2020-08-14 18:00:33 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,13 +1,15 @@
 def factorial(num):
+    fact = 1
     
     if num < 0:
        return 0
     if num == 0:
        return 1
-    fact = 1
+    
     else:
         for i in range(1,num + 1):
             fact = fact * i
         return fact
         
         
+print(factorial(4))
